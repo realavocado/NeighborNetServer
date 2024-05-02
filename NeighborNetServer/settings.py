441d9b1cf4370139ff945de4c627d9db7fd97f88
iris <a1158288818@icloud.com>
2024-05-02 10:20:08 -0400

    add message test

--- NeighborNetServer/settings.py
+++ NeighborNetServer/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'debug_toolbar',
+    'message',
 ]
 
 MIDDLEWARE = [
--- message/urls.py
+++ message/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for NeighborNetServer project.
+"""message URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
-from django.contrib import admin
-from django.urls import include, path
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('search/', include('search.urls')),
-    path('message/', include('message.urls')),
-    path("__debug__/", include("debug_toolbar.urls"))
+    path("test/", views.test),
 ]
