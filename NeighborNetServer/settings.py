755b750e6a9cf9fdf4977ff283097225d380c9e3
RealAvocado <Letian.Jiang1106@outlook.com>
2024-05-02 20:48:37 -0400

    Added some cookie configuration.

--- NeighborNetServer/settings.py
+++ NeighborNetServer/settings.py
@@ -60,10 +60,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_COOKIE_HTTPONLY = False
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
